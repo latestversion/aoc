d521b9aa3b186adf92da757c75c94775bd302c81
Niklas Bjornberg <niklasb@spotify.com>
2019-12-11 12:35:56 +0100

    Add rel mode, big mem and opcode 9

--- omegacorp.py
+++ omegacorp.py
@@ -1,15 +1,18 @@
 
 """ VirtualFury (TM), the powerful next generation Intcode processor """
+from collections import defaultdict
 
 class VirtualFury:
     STATUS_OK = 0
     STATUS_WAITING_FOR_INPUT = 1
-    STATUS_PROGRAM_ENDED = 2
+    STATUS_PROGRAM_ENDED = 99
     STATUS_EXCEPTION = 3
     STATUS_BP = 4
 
     def __init__(self, program, inputed=[]):
-        self.program = program
+        self.program = defaultdict(int)
+        for i, p in enumerate(program):
+            self.program[i] = p
         self.pc = 0
         self.output = 0
         self.num_flanks = 0
@@ -19,6 +22,8 @@ class VirtualFury:
         self.input = inputed
         self.breakpoints = []
         self.snapshot = program.copy()
+        self.relative_base = 0
+        self.out_buffer = []
 
     def save_snapshot(self):
         self.snapshot = self.program.copy()
@@ -43,15 +48,18 @@ class VirtualFury:
         assert cols > 0
 
         for row in range(0, rows):
-            s = "{} ".format(addr + row * cols)
+            s = "addr{} ".format(addr + row * cols)
             for col in range(0, cols):
                 pointer = addr + row*cols + col
-                if pointer >= len(self.program):
+                if pointer not in self.program:
                     s += " OOM "
                 else:
                     ins = self.program[pointer]
                     s += " {} ".format(ins)
             print(s)
+
+    def dump(self, addr, rows, cols=4):
+        self.dump_ints(addr, rows, cols)
 
     def bp(self, addr):
         if addr not in self.breakpoints:
@@ -83,6 +91,7 @@ class VirtualFury:
         dpc = self.execute(decoded, self.program)
 
         self.pc += dpc
+
         self.num_flanks += 1
 
     def step(self):
@@ -99,21 +108,23 @@ class VirtualFury:
         return opcode, im1, im2, im3
 
     def load(self, addr, mode):
-        if addr < 0 or addr >= len(self.program):
+        if addr < 0:
             self.status = VirtualFury.STATUS_EXCEPTION
-            raise AddressOutOfBoundsException("Illegal load for pc = {}".format(self.pc))
+            raise AddressOutOfBoundsException("Illegal load for pc = {}, addr = {}".format(self.pc, 4))
         val = self.program[addr]
         if mode == 0:
-            if val < 0 or val >= len(self.program):
+            if val < 0:
                 self.status = VirtualFury.STATUS_EXCEPTION
-                raise AddressOutOfBoundsException("Illegal load for pc = {}".format(self.pc))
+                raise AddressOutOfBoundsException("Illegal load for pc = {}, addr = {}".format(self.pc, 4))
             val = self.program[val]
+        elif mode == 2:
+            val = self.program[val + self.relative_base]
         return val
 
     def store(self, addr, val):
-        if addr < 0 or addr >= len(self.program):
+        if addr < 0:
             self.status = VirtualFury.STATUS_EXCEPTION
-            raise AddressOutOfBoundsException("Illegal store for pc = {}".format(self.pc))
+            raise AddressOutOfBoundsException("Illegal store for pc = {}, addr = {}".format(self.pc, addr))
         self.program[addr] = val
 
     def provide_input_and_run(self, inputed):
@@ -130,6 +141,8 @@ class VirtualFury:
             val1 = self.load(self.pc + 1, decoded[1])
             val2 = self.load(self.pc + 2, decoded[2])
             addr = program[self.pc + 3]
+            if decoded[3] == 2:
+                addr += self.relative_base
             self.store(addr, val1 + val2)
             # print("program[{}] = {} + {}".format(addr, val1, val2))
             return 4
@@ -137,6 +150,8 @@ class VirtualFury:
             val1 = self.load(self.pc + 1, decoded[1])
             val2 = self.load(self.pc + 2, decoded[2])
             addr = program[self.pc + 3]
+            if decoded[3] == 2:
+                addr += self.relative_base
             self.store(addr, val1 * val2)
             # print("program[{}] = {} * {}".format(addr, val1, val2))
             return 4
@@ -145,12 +160,15 @@ class VirtualFury:
                 self.status = VirtualFury.STATUS_WAITING_FOR_INPUT
                 return 0
             addr = program[self.pc + 1]
+            if decoded[1] == 2:
+                addr += self.relative_base
             self.store(addr, self.input.pop(0))
             # print("program[{}] = 5".format(addr))
             return 2
         elif opcode == 4:
             val = self.load(self.pc + 1, decoded[1])
             self.output = val
+            self.out_buffer.append(val)
             return 2
         elif opcode == 5:  # jump-if-true
             val1 = self.load(self.pc + 1, decoded[1])
@@ -171,6 +189,8 @@ class VirtualFury:
             val1 = self.load(self.pc + 1, decoded[1])
             val2 = self.load(self.pc + 2, decoded[2])
             addr = program[self.pc + 3]
+            if decoded[3] == 2:
+                addr += self.relative_base
             if val1 < val2:
                 self.store(addr, 1)
             else:
@@ -181,11 +201,18 @@ class VirtualFury:
             val1 = self.load(self.pc + 1, decoded[1])
             val2 = self.load(self.pc + 2, decoded[2])
             addr = program[self.pc + 3]
+            if decoded[3] == 2:
+                addr += self.relative_base
             if val1 == val2:
                 self.store(addr, 1)
             else:
                 self.store(addr, 0)
             return 4
+
+        elif opcode == 9:
+            val1 = self.load(self.pc + 1, decoded[1])
+            self.relative_base += val1
+            return 2
 
         elif opcode == 99:
             self.status = VirtualFury.STATUS_PROGRAM_ENDED
@@ -254,39 +281,6 @@ assert __vm.output == 0
 __vm = VirtualFury(__program.copy(), [9])
 __vm.run()
 assert __vm.output == 1
-
-__line = "8,0,20000"
-__program = list(map(int, __line.split(",")))
-
-try:
-    __vm = VirtualFury(__program.copy(), [0])
-    __vm.run()
-except Exception:
-    pass
-else:
-    assert 0
-
-__line = "1,0,1,20000"
-__program = list(map(int, __line.split(",")))
-
-try:
-    __vm = VirtualFury(__program.copy(), [0])
-    __vm.run()
-except AddressOutOfBoundsException:
-    assert __vm.status == VirtualFury.STATUS_EXCEPTION
-else:
-    assert 0
-
-__line = "101,-1,1,20000"
-__program = list(map(int, __line.split(",")))
-
-try:
-    __vm = VirtualFury(__program.copy(), [0])
-    __vm.run()
-except AddressOutOfBoundsException:
-    assert __vm.status == VirtualFury.STATUS_EXCEPTION
-else:
-    assert 0
 
 __line = "11,-1,1,20000"
 __program = list(map(int, __line.split(",")))
@@ -359,3 +353,37 @@ assert __vm.status == VirtualFury.STATUS_BP
 assert __vm.pc == 4
 __vm.run()
 assert __vm.status == VirtualFury.STATUS_PROGRAM_ENDED
+
+# Day 9 examples
+
+# Day 9 BigInt 1
+__program = [1102,34915192,34915192,7,4,7,99,0]
+__vm = VirtualFury(__program.copy())
+__vm.run()
+assert len(str(__vm.output)) == 16
+
+# Day 9 BigInt 2
+__program = [104,1125899906842624,99]
+__vm = VirtualFury(__program.copy())
+__vm.run()
+assert __vm.output == 1125899906842624
+
+
+# Day 9 BigMem
+__program = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+__vm = VirtualFury(__program.copy())
+__vm.run()
+assert __vm.out_buffer == __program
+
+# Day 9 Part 1
+__program = [1102, 34463338, 34463338, 63, 1007, 63, 34463338, 63, 1005, 63, 53, 1102, 3, 1, 1000, 109, 988, 209, 12, 9, 1000, 209, 6, 209, 3, 203, 0, 1008, 1000, 1, 63, 1005, 63, 65, 1008, 1000, 2, 63, 1005, 63, 904, 1008, 1000, 0, 63, 1005, 63, 58, 4, 25, 104, 0, 99, 4, 0, 104, 0, 99, 4, 17, 104, 0, 99, 0, 0, 1102, 1, 38, 1003, 1102, 24, 1, 1008, 1102, 1, 29, 1009, 1102, 873, 1, 1026, 1102, 1, 32, 1015, 1102, 1, 1, 1021, 1101, 0, 852, 1023, 1102, 1, 21, 1006, 1101, 35, 0, 1018, 1102, 1, 22, 1019, 1102, 839, 1, 1028, 1102, 1, 834, 1029, 1101, 0, 36, 1012, 1101, 0, 31, 1011, 1102, 23, 1, 1000, 1101, 405, 0, 1024, 1101, 33, 0, 1013, 1101, 870, 0, 1027, 1101, 0, 26, 1005, 1101, 30, 0, 1004, 1102, 1, 39, 1007, 1101, 0, 28, 1017, 1101, 34, 0, 1001, 1102, 37, 1, 1014, 1101, 20, 0, 1002, 1102, 1, 0, 1020, 1101, 0, 859, 1022, 1102, 1, 27, 1016, 1101, 400, 0, 1025, 1102, 1, 25, 1010, 109, -6, 1207, 10, 29, 63, 1005, 63, 201, 1001, 64, 1, 64, 1105, 1, 203, 4, 187, 1002, 64, 2, 64, 109, 3, 2107, 25, 8, 63, 1005, 63, 221, 4, 209, 1106, 0, 225, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -4, 2101, 0, 9, 63, 1008, 63, 18, 63, 1005, 63, 245, 1106, 0, 251, 4, 231, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 3, 2108, 38, 7, 63, 1005, 63, 273, 4, 257, 1001, 64, 1, 64, 1106, 0, 273, 1002, 64, 2, 64, 109, 22, 21102, 40, 1, 0, 1008, 1018, 40, 63, 1005, 63, 299, 4, 279, 1001, 64, 1, 64, 1106, 0, 299, 1002, 64, 2, 64, 109, -16, 21108, 41, 41, 10, 1005, 1012, 321, 4, 305, 1001, 64, 1, 64, 1105, 1, 321, 1002, 64, 2, 64, 109, 6, 2102, 1, -2, 63, 1008, 63, 22, 63, 1005, 63, 341, 1105, 1, 347, 4, 327, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 21, 1206, -8, 359, 1106, 0, 365, 4, 353, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -7, 21101, 42, 0, -6, 1008, 1016, 44, 63, 1005, 63, 389, 1001, 64, 1, 64, 1105, 1, 391, 4, 371, 1002, 64, 2, 64, 109, 2, 2105, 1, 0, 4, 397, 1106, 0, 409, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -3, 1205, 0, 427, 4, 415, 1001, 64, 1, 64, 1105, 1, 427, 1002, 64, 2, 64, 109, -13, 2102, 1, -1, 63, 1008, 63, 39, 63, 1005, 63, 449, 4, 433, 1106, 0, 453, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -10, 1202, 4, 1, 63, 1008, 63, 20, 63, 1005, 63, 479, 4, 459, 1001, 64, 1, 64, 1106, 0, 479, 1002, 64, 2, 64, 109, 7, 2108, 37, -2, 63, 1005, 63, 495, 1105, 1, 501, 4, 485, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 4, 21101, 43, 0, 1, 1008, 1010, 43, 63, 1005, 63, 523, 4, 507, 1106, 0, 527, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -4, 1208, -5, 23, 63, 1005, 63, 549, 4, 533, 1001, 64, 1, 64, 1106, 0, 549, 1002, 64, 2, 64, 109, -4, 1208, 7, 27, 63, 1005, 63, 565, 1106, 0, 571, 4, 555, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 15, 1205, 4, 587, 1001, 64, 1, 64, 1106, 0, 589, 4, 577, 1002, 64, 2, 64, 109, -7, 1202, -7, 1, 63, 1008, 63, 18, 63, 1005, 63, 613, 1001, 64, 1, 64, 1106, 0, 615, 4, 595, 1002, 64, 2, 64, 109, 5, 21107, 44, 43, 1, 1005, 1015, 635, 1001, 64, 1, 64, 1105, 1, 637, 4, 621, 1002, 64, 2, 64, 109, -2, 21102, 45, 1, 6, 1008, 1018, 44, 63, 1005, 63, 661, 1001, 64, 1, 64, 1105, 1, 663, 4, 643, 1002, 64, 2, 64, 109, -18, 1207, 6, 24, 63, 1005, 63, 685, 4, 669, 1001, 64, 1, 64, 1105, 1, 685, 1002, 64, 2, 64, 109, 4, 2101, 0, 8, 63, 1008, 63, 21, 63, 1005, 63, 707, 4, 691, 1105, 1, 711, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 17, 1206, 5, 725, 4, 717, 1105, 1, 729, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 9, 21107, 46, 47, -9, 1005, 1015, 751, 4, 735, 1001, 64, 1, 64, 1106, 0, 751, 1002, 64, 2, 64, 109, -9, 1201, -6, 0, 63, 1008, 63, 26, 63, 1005, 63, 775, 1001, 64, 1, 64, 1106, 0, 777, 4, 757, 1002, 64, 2, 64, 109, -15, 1201, 0, 0, 63, 1008, 63, 23, 63, 1005, 63, 803, 4, 783, 1001, 64, 1, 64, 1105, 1, 803, 1002, 64, 2, 64, 109, -1, 2107, 30, 10, 63, 1005, 63, 819, 1106, 0, 825, 4, 809, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, 24, 2106, 0, 5, 4, 831, 1105, 1, 843, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -5, 2105, 1, 5, 1001, 64, 1, 64, 1105, 1, 861, 4, 849, 1002, 64, 2, 64, 109, 14, 2106, 0, -5, 1105, 1, 879, 4, 867, 1001, 64, 1, 64, 1002, 64, 2, 64, 109, -17, 21108, 47, 44, 4, 1005, 1019, 899, 1001, 64, 1, 64, 1105, 1, 901, 4, 885, 4, 64, 99, 21101, 0, 27, 1, 21102, 915, 1, 0, 1106, 0, 922, 21201, 1, 58969, 1, 204, 1, 99, 109, 3, 1207, -2, 3, 63, 1005, 63, 964, 21201, -2, -1, 1, 21101, 0, 942, 0, 1105, 1, 922, 22102, 1, 1, -1, 21201, -2, -3, 1, 21101, 957, 0, 0, 1106, 0, 922, 22201, 1, -1, -2, 1106, 0, 968, 21201, -2, 0, -2, 109, -3, 2105, 1, 0]
+__vm = VirtualFury(__program.copy(), [1])
+__vm.run()
+assert __vm.output == 3235019597
+
+# Day 9 Part 2
+# __vm = VirtualFury(__program.copy(), [2])
+# __vm.run()
+# assert __vm.output == 80274
+
+print("VirtualFury Verification Suite 1.0 Ended")
